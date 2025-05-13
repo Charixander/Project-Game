f8a04cfc071e114afde075a074fcdf30e456a19e
Charixander <150956122+Charixander@users.noreply.github.com>
2025-05-13 10:00:55 -0700

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,4 +1,14 @@
 public class Room {
+  private static int roomRanks = 1;
+  private int roomRank;
 
+  public Room() {
+    roomRank = roomRanks;
+    roomRanks++;
+  }
+
+  public static void resetRanks() {
+    roomRanks = 1;
+  }
   
 }
