3c1811c4e6a7ba91c7b8ebeac292537693649262
socks <s170430@scusd.net>
2025-05-13 09:19:33 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,4 +1,28 @@
 public class Item {
 
+  String name;
+  String description;
+  int level;
+
+  public Item (String name)
+  {
+    this.name = name;
+    description = "An item.";
+    level = 1;
+  }
+
+  public Item (String name, String description)
+  {
+    this.name = name;
+    this.description = description;
+    level = 1;
+  }
+
+  public Item (String name, String description, int level)
+  {
+    this.name = name;
+    this.description = description;
+    this.level = level;
+  }
   
 }
