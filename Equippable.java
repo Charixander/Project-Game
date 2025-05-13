96012bbce3bf8ab1d8067cbb31585e2cb0dff403
socks <s170430@scusd.net>
2025-05-13 09:38:41 -0700

    Update Equippable.java

--- Equippable.java
+++ Equippable.java
@@ -1,4 +1,26 @@
 public class Equippable extends Item {
 
+  private int baseAttack;
+  private int baseDefense;
+
+  public Equippable () 
+  {
+    baseAttack = 0;
+    baseDefense = 0;
+  }
+
+  public Equippable (int atk, int def)
+  {
+    baseAttack = atk;
+    baseDefense = def;
+  }
+
+  public Equippable (String name, String description, int level, int atk, int def)
+  {
+    super(name, description, level);
+    baseAttack = atk;
+    baseDefense = def;
+  }
+
   
 }
