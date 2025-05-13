60db2304ab13fff5b269016db951b012b1f86109
1234567890Coder1234567890 <s163683@scusd.net>
2025-05-13 10:07:53 -0700

    Update Room.java
    room desc

--- Room.java
+++ Room.java
@@ -1,10 +1,16 @@
 public class Room {
   private static int roomRanks = 1;
   private int roomRank;
-
+  private String[] cleanliness = {"musty", "rancid", "clean", "spotless", "decayed", "moldy", "gross", "boring", "golden"};
+  private String[] second = {"You can barely see ahead of you.", "The buzzing of lights dig into your head", "The janitor must've missed a spot.", "Have people been here before?"};
+  private String[] lights = {"flickering candlelight", "office lights", "a glowing bonfire", "spots of sunlight scattered from the cieling"};
+  private String roomDesc;
+  
+  
   public Room() {
     roomRank = roomRanks;
     roomRanks++;
+    roomDesc = "A " + cleanliness[(int) Math.random() * 9] + " room with " + lights[(int) Math.random() * 4] + ". " + second[(int) Math.random() * 4];
   }
 
   public static void resetRanks() {
@@ -12,7 +18,7 @@ public class Room {
   }
 
   public String getDesc() {
-    return roomDesc
+    return roomDesc;
   }
   
 }
