de6d7a52459f1a54535d0101388be6813ac57e3c
socks <s170430@scusd.net>
2025-05-15 09:28:38 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
 
-    Room a = new Room();
-    System.out.print(a.getRoom());
-    
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What is your name?");
+    String userName = input.nextLine();
+    Player player = new Player(userName);
+    Enemy enemy = new Enemy("billy");
+
+    Combat test = new Combat(player, enemy);
+
+    test.combatBegin();
   }
 }
