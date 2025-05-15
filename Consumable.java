e27a9e772f17e1fd63b2c3631de5899c5a36b0a5
socks <s170430@scusd.net>
2025-05-15 09:45:58 -0700

    Update Consumable.java

--- Consumable.java
+++ Consumable.java
@@ -1,32 +1,24 @@
 public class Consumable extends Item {
 
   private int healthRestore;
-  private int attackBoost;
-  private int defenseBoost;
   private int turnDuration;
 
   public Consumable ()
   {
     healthRestore = 1;
-    attackBoost = 0;
-    defenseBoost = 0;
     turnDuration = 1;
   }
 
   public Consumable (int hp)
   {
     healthRestore = hp;
-    attackBoost = 0;
-    defenseBoost = 0;
     turnDuration = 1;
   }
 
-  public Consumable (String name, String description, int level, int hp, int atk, int def, int turns)
+  public Consumable (String name, String description, int level, int hp, int turns)
   {
     super(name, description, level);
     healthRestore = hp;
-    attackBoost = atk;
-    defenseBoost = def;
     turnDuration = turns;
     if (turnDuration < 1)
     {
@@ -34,5 +26,10 @@ public class Consumable extends Item {
     }
   }
 
+  public void consume(Player player)
+  {
+    player.addHealth(healthRestore);
+    turnDuration--;
+  }
   
 }
