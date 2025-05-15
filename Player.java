1f440e7ef1dfdc3e9554c5c1d94c0b5daeec858b
socks <s170430@scusd.net>
2025-05-15 09:56:29 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,8 +3,6 @@ public class Player extends Mob {
   private int level;
   private double xp;
   Item[] inventory = new Item[10];
-  private Equippable armor;
-  private Equippable weapon;
   private int money;
 
   public Player ()
@@ -12,8 +10,6 @@ public class Player extends Mob {
     level = 1;
     xp = 0.0;
     inventory = new Item[10];
-    armor = null;
-    weapon = null;
     money = 0;
   }
 
@@ -23,19 +19,15 @@ public class Player extends Mob {
     level = 1;
     xp = 0.0;
     inventory = new Item[10];
-    armor = null;
-    weapon = null;
     money = 0;
   }
 
-  public Player (String name, int HP, int ATK, int DEF)
+  public Player (String name, int HP, int ATK, int DEF, Equippable weapon, Equippable armor)
   {
-    super(name, HP, ATK, DEF);
+    super(name, HP, ATK, DEF, weapon, armor);
     level = 1;
     xp = 0.0;
     inventory = new Item[10];
-    armor = null;
-    weapon = null;
     money = 0;
   }
 }
