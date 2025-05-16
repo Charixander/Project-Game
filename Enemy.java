cb0f60e97692c1969f2990780275c1523de9a08a
socks <s170430@scusd.net>
2025-05-15 22:06:50 -0700

    Update Enemy.java
    Updated with Combat.java update

--- Enemy.java
+++ Enemy.java
@@ -16,13 +16,18 @@ public class Enemy extends Mob {
     moneyReward = 0;
   }
 
+  public Enemy (String name, int HP, int ATK, int DEF)
+  {
+    super(name, HP, ATK, DEF, null, null);
+    xpReward = 0;
+    moneyReward = 0;
+  }
+
   public Enemy (String name, int HP, int ATK, int DEF, Equippable weapon, Equippable armor)
   {
     super(name, HP, ATK, DEF, weapon, armor);
     xpReward = 0;
     moneyReward = 0;
-    weapon = null;
-    armor = null;
   }
 
   public Enemy (String name, int HP, int ATK, int DEF, int xp, int money, Equippable weapon, Equippable armor)
