614eb741af15a22920aa7a26a474a08c2ee27ad4
socks <s170430@scusd.net>
2025-05-15 22:05:24 -0700

    Update Player.java
    Updated with Combat.java update

--- Player.java
+++ Player.java
@@ -22,6 +22,15 @@ public class Player extends Mob {
     money = 0;
   }
 
+  public Player (String name, int HP, int ATK, int DEF)
+  {
+    super(name, HP, ATK, DEF, null, null);
+    level = 1;
+    xp = 0.0;
+    inventory = new Item[10];
+    money = 0;
+  }
+
   public Player (String name, int HP, int ATK, int DEF, Equippable weapon, Equippable armor)
   {
     super(name, HP, ATK, DEF, weapon, armor);
@@ -29,5 +38,62 @@ public class Player extends Mob {
     xp = 0.0;
     inventory = new Item[10];
     money = 0;
+  }
+
+  public Item[] getInventory()
+  {
+    return inventory;
+  }
+
+  public void setInventory(Item item, int index)
+  {
+    inventory[index-1] = item;
+  }
+
+  public void updateInventory()
+  {
+    Item[] newInventory = new Item[inventory.length];
+
+    int index = 0;
+
+    for (int i = 0; i < inventory.length; i++)
+    {
+      if (inventory[i] != null)
+      {
+        newInventory[index] = inventory[i];
+        index++;
+      }
+    }
+
+    inventory = newInventory;
+  }
+
+  public String openInventory()
+  {
+    String text = "";
+    for (int i = 0; i < 5; i++)
+    {
+      if (inventory[i] == null)
+      {
+        text += "| N/A (" + (i+1) + ")|";
+      }
+      else 
+      {
+        text += "| " + inventory[i].getName() + "(" + (i+1) + ") |";
+      }
+    }
+    text += "\n";
+    for (int j = 5; j < 10; j++)
+    {
+      if (inventory[j] == null)
+      {
+        text += "| N/A (" + (j+1) + ")|";
+      }
+      else 
+      {
+        text += "| " + inventory[j].getName() + "(" + (j+1) + ") |";
+      }
+    }
+    return text;
   }
 }
