926bdf22d7d1fa31ec6eaa1eeb560293c76c0ba3
socks <s170430@scusd.net>
2025-05-19 19:58:43 -0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -2,11 +2,15 @@ public class Enemy extends Mob {
 
   private int xpReward;
   private int moneyReward;
+  private int healSelf;
+  private int healOthers;
 
   public Enemy ()
   {
     xpReward = 0;
     moneyReward = 0;
+    healSelf = 0;
+    healOthers = 0;
   }
 
   public Enemy (String name)
@@ -14,27 +18,74 @@ public class Enemy extends Mob {
     super(name);
     xpReward = 0;
     moneyReward = 0;
+    healSelf = 0;
+    healOthers = 0;
   }
 
-  public Enemy (String name, int HP, int ATK, int DEF)
+  public Enemy (String name, int HP, int ATK, int DEF, int xp, int money)
   {
     super(name, HP, ATK, DEF, null, null);
-    xpReward = 0;
-    moneyReward = 0;
+    xpReward = xp;
+    moneyReward = money;
+    healSelf = 0;
+    healOthers = 0;
   }
 
-  public Enemy (String name, int HP, int ATK, int DEF, Equippable weapon, Equippable armor)
+  public Enemy (String name, int HP, int ATK, int DEF, int xp, int money, int healSelf, int healOthers)
   {
-    super(name, HP, ATK, DEF, weapon, armor);
-    xpReward = 0;
-    moneyReward = 0;
+    super(name, HP, ATK, DEF, null, null);
+    xpReward = xp;
+    moneyReward = money;
+    this.healSelf = healSelf;
+    this.healOthers = healOthers;
   }
 
-  public Enemy (String name, int HP, int ATK, int DEF, int xp, int money, Equippable weapon, Equippable armor)
+  public Enemy (String name, int HP, int ATK, int DEF, int xp, int money, Equippable weapon, Equippable armor, int healSelf, int healOthers)
   {
     super(name, HP, ATK, DEF, weapon, armor);
     xpReward = xp;
     moneyReward = money;
+    this.healSelf = healSelf;
+    this.healOthers = healOthers;
   }
-  
+
+  public int getXP()
+  {
+    return xpReward;
+  }
+
+  public int getMoney()
+  {
+    return moneyReward;
+  }
+
+  public boolean canHealSelf()
+  {
+    return healSelf > 0;
+  }
+
+  public boolean canHealOthers()
+  {
+    return healOthers > 0;
+  }
+
+  public int healSelfAmount()
+  {
+    return healSelf;
+  }
+
+  public int healOtherAmount()
+  {
+    return healOthers;
+  }
+
+  public void healSelf()
+  {
+    super.addHealth(healSelf);
+  }
+
+  public void healOther(Enemy e)
+  {
+    e.addHealth(healOthers);
+  }
 }
