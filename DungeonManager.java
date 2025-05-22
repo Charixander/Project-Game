5bec83307b62ba8677acd76d765963c18717efd1
Charixander <150956122+Charixander@users.noreply.github.com>
2025-05-22 10:08:22 -0700

    Update DungeonManager.java

--- DungeonManager.java
+++ DungeonManager.java
@@ -1,2 +1,18 @@
 public class DungeonManager {
+  private Player player;
+  private Floor[] floors;
+  private int roomCount;
+  private static boolean gameAlive = true;
+
+  public DungeonManager(int floorCount, int roomCount) {
+    floors = New Floor[5];
+  }
+
+  public static boolean getGameAlive() {
+    return gameAlive;
+  }
+
+  public static void gameOver() {
+    gameAlive = false;
+  }
 }
