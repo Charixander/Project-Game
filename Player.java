d5aaedf201e3690837d8917388f7950b5ee9484b
socks <s170430@scusd.net>
2025-05-22 20:00:29 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Player extends Mob {
 
   private int level;
@@ -94,6 +96,85 @@ public class Player extends Mob {
     super.setDEF((int) temp);
 
     System.out.println("You leveled up!\n" + (level-1) + " --> " + level + "\n");
+  }
+
+  public boolean checkInventory()
+  {
+    boolean flag = false;
+    updateInventory();
+    for (int i = 0; i < inventory.length; i++)
+    {
+      if (inventory[i] == null)
+      {
+        flag = true;
+        break;
+      }
+    }
+    return flag;
+  }
+
+  private void replaceInventory(Item item)
+  {
+    Scanner input = new Scanner(System.in);
+    updateInventory();
+
+    while (true)
+    {
+      System.out.println("\n" + openInventory() + "\nInsert the number value for what item you wish to replace with " + item.getName() + ".");
+      System.out.println("Type 'cancel' or 'back' to exit out.\n");
+      int n = -1;
+      String temp = input.nextLine();
+      if (temp.matches("-?\\d+"))
+      {
+        n = Integer.parseInt(temp);
+      }
+      if (temp.toLowerCase().equals("cancel") || temp.toLowerCase().equals("back"))
+      {
+        System.out.println("\nYou discarded " + item.getName() + ".\n");
+        break;
+      }
+      else if (n >= 1 && n <= 10)
+      {
+        System.out.println("\nAre you sure you want to replace " + inventory[n-1].getName() + "?");
+        System.out.println("(Yes/No)");
+        temp = input.nextLine();
+        if (temp.equals("y") || temp.equals("yes"))
+        {
+          System.out.println("\n" + item.getName() + " was added to your inventory.");
+          System.out.println(inventory[n-1].getName() + " was discarded.\n");
+          setInventory(item, n);
+          break;
+        }
+        else
+        {
+          
+        }
+      }
+      else 
+      {
+        System.out.println("Invalid input!\n");
+      }
+    }
+    input.close();
+    
+  }
+  
+  public void addInventory(Item item)
+  {
+    updateInventory();
+    boolean flag = true;
+    for (int i = 0; i < inventory.length; i++)
+    {
+      if (inventory[i] == null)
+      {
+        inventory[i] = item;
+        flag = false;
+      }
+    }
+    if (flag)
+    {
+      replaceInventory(item);
+    }
   }
 
   public Item[] getInventory()
