9b7914ee9f208f5023efe182f02f42a84ba5089f
Charixander <150956122+Charixander@users.noreply.github.com>
2025-05-22 21:23:57 -0700

    Update Floor.java

--- Floor.java
+++ Floor.java
@@ -1,22 +1,80 @@
 public class Floor {
   private Room[] rooms;
-  private static int floorLevel = 1;
+  private static int floorLevel = 0;
   
   public Floor()  {
-    rooms = new Room[10];
+    floorLevel++;
+    rooms = new Room[5];
+    Room.resetRank();
+    int random = 0;
+    for(int i = 0; i < rooms.length-1; i++) {
+      random = (int)(Math.random()*10);
+      if(random <= 1) {
+        rooms[i] = new TreasureRoom(floorLevel);
+      } else {
+        rooms[i] = new CombatRoom(floorLevel);
+      }
+    }
+    rooms[rooms.length-1] = new BossRoom(floorLevel, false);
+  }
+
+  public Floor(int RoomCount)  {
+    floorLevel++;
+    rooms = new Room[RoomCount];
     Room.resetRank();
     int random = 0;
     for(int i = 0; i < rooms.length; i++) {
       random = (int)(Math.random()*10);
       if(random <= 1) {
-        System.out.print("Need to add TreasureRoom");
+        rooms[i] = new TreasureRoom(floorLevel);
       } else {
         rooms[i] = new CombatRoom(floorLevel);
       }
     }
-    floorLevel++;
+    rooms[rooms.length-1] = new BossRoom(floorLevel, false);
   }
 
+  public Floor(boolean isFinalFloor)  {
+    floorLevel++;
+    rooms = new Room[5];
+    Room.resetRank();
+    int random = 0;
+    for(int i = 0; i < rooms.length-1; i++) {
+      random = (int)(Math.random()*10);
+      if(random <= 1) {
+        rooms[i] = new TreasureRoom(floorLevel);
+      } else {
+        rooms[i] = new CombatRoom(floorLevel);
+      }
+    }
+    rooms[rooms.length-1] = new BossRoom(floorLevel, isFinalFloor);
+  }
+
+  public Floor(int RoomCount, boolean isFinalFloor)  {
+    floorLevel++;
+    rooms = new Room[RoomCount];
+    Room.resetRank();
+    int random = 0;
+    for(int i = 0; i < rooms.length-1; i++) {
+      random = (int)(Math.random()*10);
+      if(random <= 1) {
+        rooms[i] = new TreasureRoom(floorLevel);
+      } else {
+        rooms[i] = new CombatRoom(floorLevel);
+      }
+    }
+    rooms[rooms.length-1] = new BossRoom(floorLevel, isFinalFloor);
+  }
+
+  public String getRoomContents() {
+    String contents = "";
+    for(int i = 0; i < rooms.length; i++) {
+      contents += "room " + (i+1) + ": \n";
+      contents += rooms[i].getRoomContents() + "\n\n";
+    }
+    return contents;
+  }  
+  
   public static int getLevel()
   {
     return floorLevel;
