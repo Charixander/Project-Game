246c317634bdc430b49271150a47e4b18ec2a8e8
Charixander <150956122+Charixander@users.noreply.github.com>
2025-05-23 03:46:56 -0700

    Update TreasureRoom.java

--- TreasureRoom.java
+++ TreasureRoom.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
+/*
+* Room with a item in it.
+* designed to give player a break and chance to comeback.
+* player doesn't know it's a treasure room until they investigate.
+*/
+
+
 public class TreasureRoom extends Room {
+    //the treasure they recieve.
     Equippable treasure;
 
+    //contructor
     public TreasureRoom(int floorLevel) {
       super(floorLevel);
+      //randomStat so quality of item is random
       int randomStat = (int)(Math.random()*floorLevel*floorLevel*5) + 5;
       int random = (int)(Math.random()*2);
       if(random == 0) {
@@ -17,7 +27,14 @@ public class TreasureRoom extends Room {
     public String getRoomContents() {
     return "a chest with a " + treasure.getName() + "!";
   }
-  
+
+    /*
+    *Simulates the room.
+    * Formatted same way as CombatRoom so player
+    * Doesn't know if they're in a TreasureRoom or
+    * CombatRoom.
+    * Purpose is to encourage exploration and relieve players with a bonus.
+    */
    public void runRoom(Player player) {
      System.out.println(getDesc());
      Scanner input = new Scanner(System.in);
@@ -34,9 +51,21 @@ public class TreasureRoom extends Room {
         } else if(action.toLowerCase().equals("search")) {
         flag = false;
         System.out.println("You Found Treasure!!!! There was " + getRoomContents());
-        //UPDATE HERE
-      } else if(action.toLowerCase().equals("inventory")) {
-        //UPDATE HERE
+        player.addInventory(treasure);
+         boolean newFlag = true;
+          while(newFlag) {
+            System.out.println("The room is now empty, what do you want to do now?");
+            System.out.println("\n|| [ Leave ] || [ Inventory ] ||\n");
+            action = input.nextLine();
+            if(action.toLowerCase().equals("leave")) {
+                newFlag = false;
+               System.out.println("You move on to the next room.");
+            } else if(action.toLowerCase().equals("inventory")) { 
+              player.hud();
+            }
+          }
+         } else if(action.toLowerCase().equals("inventory")) {
+            player.hud();
       } else {
         System.out.println("invalid Input.");
       }
